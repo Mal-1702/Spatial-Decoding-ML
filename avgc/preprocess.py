5ef3b95323d863dc3ffe965ca4f1c8986bb8c118
Mal-1702 <Malhaarphadtare17@gmail.com>
2026-09-17 01:11:00 +0530

    Add signal views for gaze-control variants and HEOG

--- avgc/preprocess.py
+++ avgc/preprocess.py
@@ -21,3 +21,31 @@ def regress_out_eog(eeg, exg):
     design = np.c_[exg, np.ones(len(exg))]
     coef, *_ = np.linalg.lstsq(design, eeg, rcond=None)
     return eeg - design @ coef
+
+
+def views(trial, eeg_labels):
+    """Return {view_name: (T, n_channels) array} for the spatial decoders.
+
+    EOG            4 EOG electrodes only                        -> gaze baseline
+    EEG            64 EEG, common average reference, 1-40 Hz
+    EEG_eogreg     EOG regressed out, then CAR
+    EEG_posterior  29 posterior channels, referenced to their own mean
+                   (a whole-head CAR would smear frontal eye artifacts into them)
+    EEG_alpha      CAR, 8-13 Hz (alpha lateralisation band)
+    EEG_gazectrl   EOG regression + posterior + alpha, all three controls combined
+    """
+    post = [eeg_labels.index(ch) for ch in C.POSTERIOR]
+    clean = regress_out_eog(trial.eeg, trial.exg)
+    return {
+        "EOG": trial.exg,
+        "EEG": common_average(trial.eeg),
+        "EEG_eogreg": common_average(clean),
+        "EEG_posterior": common_average(trial.eeg[:, post]),
+        "EEG_alpha": bandpass(common_average(trial.eeg), C.ALPHA_BAND),
+        "EEG_gazectrl": bandpass(common_average(clean[:, post]), C.ALPHA_BAND),
+    }
+
+
+def heog(trial):
+    i, j = C.EXG.index(C.HEOG[0]), C.EXG.index(C.HEOG[1])
+    return trial.exg[:, i] - trial.exg[:, j]
